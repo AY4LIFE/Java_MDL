b3903bd6f03547e827eb206fad0d021adfad7436
Ayomide Akinsanya <ayomide.akinsanya@ucalgary.ca>
2025-04-18 08:25:57 -0600

    Created our Table View with columns

--- src/main/java/com/example/mdl/HelloController.java
+++ src/main/java/com/example/mdl/HelloController.java
@@ -1,14 +1,16 @@
 package com.example.mdl;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    ObservableList<EmergencyReport> data = FXCollections.observableArrayList();
+    @FXML private TableView<String> EmergencyTable = new TableView<>();
+    @FXML private TableColumn<EmergencyReport, EmergencyReport> Type = new TableColumn<>();
+    @FXML private TableColumn<EmergencyReport, EmergencyReport> Location = new TableColumn<>();
+    @FXML private TableColumn<EmergencyReport, EmergencyReport> Priority = new TableColumn<>();
 }
