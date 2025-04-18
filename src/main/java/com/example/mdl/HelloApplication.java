d2ad87ff062478739c8bae8f13ddd663e5e95011
Ayomide Akinsanya <ayomide.akinsanya@ucalgary.ca>
2025-04-18 08:24:40 -0600

    Change of Title

--- src/main/java/com/example/mdl/HelloApplication.java
+++ src/main/java/com/example/mdl/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Emergency.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Emergency Report 2025");
         stage.setScene(scene);
         stage.show();
     }
